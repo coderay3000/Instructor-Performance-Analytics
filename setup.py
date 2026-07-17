305f43ae74b1dd41e69361d718bca3bd8bf583aa
Ayush yadav <ayush1337yadav@gmail.com>
2026-07-17 11:56:07 +0530

    data uploaded to mongo_db

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="src",
-    version="0.0.1",
+    name="instructor_performance_analytics",
+    version="0.1.0",
     author="Ayush Yadav",
     author_email="ayush1337yadav@gmail.com",
-    packages=find_packages()
+    description="End-to-End Instructor Performance Analytics using Machine Learning and MLOps",
+    packages=find_packages(),
+    python_requires=">=3.10"
 )
